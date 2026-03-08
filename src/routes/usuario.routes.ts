9386f08576a713e707340db3c11d030eff31d3f8
Lucas Torres <eu.lucastr@gmail.com>
2026-03-08 16:25:31 -0300

    feat: add swagger docs

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -5,7 +5,65 @@ import type { IUsuarioCreate } from "../app/interfaces/usuario.interface.js";
 export async function usuarioRoutes(fastify: FastifyInstance) {
   const usuarioUseCase = new UsuarioUseCase();
   
-  fastify.post<{Body: IUsuarioCreate}>('/', async (req, reply) => {
+  fastify.get('/', {
+    schema: {
+      description: 'Listar todos os usuários',
+      tags: ['Usuários'],
+      response: {
+        200: {
+          description: 'Lista de usuários',
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+          }
+        }
+      }
+    }
+  }, (req, reply) => {
+    return reply.send({ message: 'Listar usuários' });
+  })
+  
+  fastify.post<{ Body: IUsuarioCreate }>('/', {
+    schema: {
+      description: 'Criar um novo usuário',
+      tags: ['Usuários'],
+      body: {
+        type: 'object',
+        required: ['nome', 'email'],
+        properties: {
+          nome: { type: 'string' },
+          email: { type: 'string' }
+        }
+      },
+      response: {
+        201: {
+          description: 'Usuário criado com sucesso',
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+            data: {
+              type: 'object',
+              properties: {
+                id: { type: 'string' },
+                nome: { type: 'string' },
+                email: { type: 'string' },
+                criado_em: { type: 'string', format: 'date-time' },
+                atualizado_em: { type: 'string', format: 'date-time' }
+              }
+            }
+          }
+        },
+        500: {
+          description: 'Erro ao criar usuário',
+          type: 'object',
+          properties: {
+            message: { type: 'string' },
+            error: { type: 'string' }
+          }
+        }
+      }
+    }
+  }, async (req, reply) => {
     try {
       const { nome, email } = req.body;
       const data = await usuarioUseCase.create({
@@ -19,9 +77,5 @@ export async function usuarioRoutes(fastify: FastifyInstance) {
       console.error(error);
       return reply.status(500).send({ message: 'Erro ao criar usuário', error });
     }
-  })
-  
-  fastify.get('/', (req, reply) => {
-    return reply.send({ message: 'Listar usuários' });
   })
 } 
